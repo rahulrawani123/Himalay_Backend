9637e52f9e4da16f1b17bf03c114d9021b95f4cb
Rahul Kumar Rawani <147590423+rahulrawani123@users.noreply.github.com>
2025-06-24 00:36:02 +0530

    d

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/Himalay");
+    await mongoose.connect("mongodb+srv://himalayfan:j8YM0uQcPCx7gJRd@cluster0.bxqj3m0.mongodb.net/Himalay?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
